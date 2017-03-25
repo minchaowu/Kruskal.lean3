baef87ce018a60c896c9a2c3fd0355759cd05c57
Minchao Wu <logic.mcwu@gmail.com>
2017-03-24 20:58:16 -0400

    refactor(dickson)

--- dickson.lean
+++ dickson.lean
@@ -1,16 +1,16 @@
-import data.set tools.super
-open classical fin nat function prod subtype set super
+/-
+Copyright (c) 2016 Minchao Wu. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Minchao Wu
+-/
+
+import tools.super
+open classical nat function prod subtype super
 
 noncomputable theory
 
 theorem lt_or_eq_of_lt_succ {n m : ℕ} (H : n < succ m) : n < m ∨ n = m := 
 lt_or_eq_of_le (le_of_lt_succ H)
-
-theorem imp_of_not_and {p q : Prop} (H : ¬ (p ∧ q)) : p → ¬ q := by super
--- assume Hp Hq, H (and.intro Hp Hq)
-
-theorem not_and_of_imp {p q : Prop} (H : p → ¬ q) : ¬ (p ∧ q) := by super
--- assume H1, have ¬ q, from H (and.left H1), this (and.right H1)
 
 theorem and_of_not_imp {p q : Prop} (H : ¬ (p → ¬ q)) : p ∧ q := by super
 
@@ -36,9 +36,9 @@ proposition le_trans {A : Type} [H : quasiorder A] {a b c : A} (H₁ : a ≤ b) 
 structure [class] wqo  (A : Type) extends quasiorder A : Type :=
 (is_good : ∀ f : ℕ → A,  ∃ i j : ℕ, i < j ∧ le (f i) (f j))
 
-definition is_good {A : Type} (f : ℕ → A) (o : A → A → Prop) := ∃ i j : ℕ, i < j ∧ o (f i) (f j)
+def is_good {A : Type} (f : ℕ → A) (o : A → A → Prop) := ∃ i j : ℕ, i < j ∧ o (f i) (f j)
 
-definition o_for_pairs {A B : Type} (o₁ : A → A → Prop) (o₂ : B → B → Prop) (s : A × B) (t : A × B) := 
+def o_for_pairs {A B : Type} (o₁ : A → A → Prop) (o₂ : B → B → Prop) (s : A × B) (t : A × B) := 
 o₁ (s^.1) (t^.1) ∧ o₂ (s^.2) (t^.2) 
 
 instance qo_prod  {A B: Type} [o₁ : quasiorder A] [o₂ : quasiorder B] : quasiorder (A × B) :=
@@ -48,7 +48,7 @@ have trans : ∀ a b c, op a b → op b c → op a c, from λ x y z h1 h2,
 ⟨(quasiorder.trans h1^.left h2^.left), quasiorder.trans h1^.right h2^.right⟩,
 show _, from quasiorder.mk op refl trans
 
-definition terminal {A : Type} (o : A → A → Prop) (f : ℕ → A) (m : ℕ) := ∀ n, m < n → ¬ o (f m) (f n)
+def terminal {A : Type} (o : A → A → Prop) (f : ℕ → A) (m : ℕ) := ∀ n, m < n → ¬ o (f m) (f n)
 
 theorem prop_of_non_terminal {A : Type} {o : A → A → Prop} {f : ℕ → A} {m : ℕ} (H : ¬ @terminal _ o f m) : ∃ n, m < n ∧ o (f m) (f n) :=
 let ⟨n,h⟩ := exists_not_of_not_forall H in ⟨n,(and_of_not_imp h)⟩
@@ -61,53 +61,46 @@ parameter f : ℕ → A
   section
   parameter H : ∀ N, ∃ r, N < r ∧ terminal wqo.le f r
   
-  definition find_terminal_index (n : ℕ) : {x : ℕ // n < x ∧ terminal wqo.le f x} := 
-  nat.rec_on n (let i := some (H 0) in tag i (some_spec (H 0)))
+  def terminal_index (n : ℕ) : {x : ℕ // n < x ∧ terminal wqo.le f x} := 
+  nat.rec_on n (let i := some (H 0) in ⟨i, (some_spec (H 0))⟩)
   (λ pred index', 
-   let i' := (elt_of index') in
+   let i' := index'^.elt_of in
    let i := some (H i') in
    have p : i' < i ∧ terminal wqo.le f i, from some_spec (H i'),
-   have lt : i' < i, from and.left p,
    have pred < i', from (has_property index')^.left,
-   have succ pred ≤ i', from succ_le_of_lt this,
-   have succ pred < i, from lt_of_le_of_lt this lt,
-   have succ pred < i ∧ terminal wqo.le f i, from and.intro this (and.right p),
-   tag i this)
-  
-  lemma increasing_fti {n m : ℕ} : n < m → elt_of (find_terminal_index n) < elt_of (find_terminal_index m) :=
-  nat.induction_on m
-  (assume H, absurd H dec_trivial)
+   have succ pred < i, from lt_of_le_of_lt this p^.left, 
+   ⟨i, ⟨this,p^.right⟩⟩)
+
+  lemma increasing_fti {n m : ℕ} : n < m → elt_of (terminal_index n) < elt_of (terminal_index m) :=
+  nat.rec_on m (assume H, absurd H dec_trivial)
   (take a, assume IH, assume lt,
    have disj : n < a ∨ n = a, from lt_or_eq_of_lt_succ lt,
-   have elt_of (find_terminal_index a) < elt_of (find_terminal_index (succ a)), from (some_spec (H (elt_of (find_terminal_index a))))^.left,
-   or.elim disj
-   (λ Hl, lt.trans (IH Hl) this)
-   (λ Hr, by rw Hr;exact this))
+   have (terminal_index a)^.elt_of < (terminal_index (succ a))^.elt_of, from
+     (some_spec $ H (terminal_index a)^.elt_of)^.left,
+   or.elim disj (λ Hl, lt.trans (IH Hl) this) (λ Hr, by rw Hr;exact this))
 
-  private definition g (n : ℕ) := f (elt_of (find_terminal_index n))
+  private def g (n : ℕ) := f (terminal_index n)^.elt_of
 
-  theorem terminal_g (n : ℕ) : terminal wqo.le g n := 
-  have ∀ n', (elt_of (find_terminal_index n)) < n' → ¬ wqo.le (f (elt_of (find_terminal_index n))) (f n'), from and.right (has_property (find_terminal_index n)),
-  take n', assume h, this (elt_of (find_terminal_index n')) (increasing_fti h)
+  lemma terminal_g (n : ℕ) : terminal wqo.le g n := 
+  have ∀ n', (terminal_index n)^.elt_of < n' → ¬ wqo.le (f (terminal_index n)^.elt_of) (f n'), from (has_property (terminal_index n))^.right,
+  take n', assume h, this (terminal_index n')^.elt_of (increasing_fti h)
 
-  theorem bad_g : ¬ is_good g wqo.le := 
+  lemma bad_g : ¬ is_good g wqo.le := 
   have H1 : ∀ i j, i < j → ¬ wqo.le (g i) (g j), from λ i, λ j, λ h, (terminal_g i) j h,
   suppose ∃ i j, i < j ∧ wqo.le (g i) (g j),
   let ⟨i,j,h⟩ := this in
   have ¬ wqo.le (g i) (g j), from H1 i j (and.left h),
   show _, from this (and.right h)
 
-  theorem local_contradiction : false := bad_g (wqo.is_good g)
+  lemma local_contradiction : false := bad_g (wqo.is_good g)
 
   end
 
 theorem finite_terminal : ∃ N, ∀ r, N < r → ¬ terminal wqo.le f r := 
 have ¬ ∀ N, ∃ r, N < r ∧ @terminal A wqo.le f r, by apply local_contradiction,
-have ∃ N, ¬ ∃ r, N < r ∧ @terminal A wqo.le f r, from exists_not_of_not_forall this,
-let ⟨n,h⟩ := this in
-have ∀ r, ¬ (n < r ∧ @terminal A wqo.le f r), by super,-- by rw forall_iff_not_exists at h;exact h,
-have ∀ r, n < r → ¬ @terminal A wqo.le f r, from λ r, imp_of_not_and (this r),
-show _, from exists.intro n this
+have ∃ N, ¬ ∃ r, N < r ∧ @terminal A wqo.le f r, by super,
+let ⟨n,h⟩ := this in have ∀ r, n < r → ¬ @terminal A wqo.le f r, by super,
+⟨n,this⟩
 
 end
 
@@ -124,79 +117,67 @@ parameter [wqo B]
   theorem  finite_terminal_on_A : ∃ N, ∀ r, N < r → ¬ @terminal A wqo.le (fst ∘ f) r := 
   finite_terminal (fst ∘ f)
 
-  definition sentinel := some finite_terminal_on_A
+  def sentinel := some finite_terminal_on_A
 
-  definition h_helper (n : ℕ) : {x : ℕ // sentinel < x ∧ ¬ @terminal A wqo.le (fst ∘ f) x} :=
+  def h_helper (n : ℕ) : {x : ℕ // sentinel < x ∧ ¬ @terminal A wqo.le (fst ∘ f) x} :=
   nat.rec_on n
   (have ∃ m, sentinel < m, by apply existence_of_nat_gt,
-  let i := some this in 
-  have ge : sentinel < i, from some_spec this,
+  let i := some this in have ge : sentinel < i, from some_spec this,
   have ¬ @terminal A wqo.le (fst ∘ f) i, from (some_spec finite_terminal_on_A) i ge,
-  have sentinel < i ∧ ¬ terminal wqo.le (fst ∘ f) i, from  and.intro ge this,
-  tag i this)
-  (λ pred h', 
-  let i' := elt_of h' in
+  have sentinel < i ∧ ¬ terminal wqo.le (fst ∘ f) i, from ⟨ge,this⟩,
+  ⟨i, this⟩)
+  (λ pred h', let i' := elt_of h' in
   have lt' : sentinel < i', from (has_property h')^.left,
-  have ¬ terminal wqo.le (fst ∘ f) i', from and.right (has_property h'),
+  have ¬ terminal wqo.le (fst ∘ f) i', from (has_property h')^.right,
   have ∃ n, i' < n ∧ ((fst ∘ f) i') ≤ ((fst ∘ f) n), from prop_of_non_terminal this,
-  let i := some this in
-  have i' < i, from (some_spec this)^.left,
+  let i := some this in have i' < i, from (some_spec this)^.left,
   have lt : sentinel < i, from lt.trans lt' this,
   have ∀ r, sentinel < r → ¬ terminal wqo.le (fst ∘ f) r, from some_spec finite_terminal_on_A,
   have ¬ terminal wqo.le (fst ∘ f) i, from this i lt,
-  have sentinel < i ∧ ¬ terminal wqo.le (fst ∘ f) i, from and.intro lt this,
-  tag i this)
+  have sentinel < i ∧ ¬ terminal wqo.le (fst ∘ f) i, from ⟨lt,this⟩,
+  ⟨i,this⟩)
 
-  private definition h (n : ℕ) := elt_of (h_helper n)
+  private def h (n : ℕ) := (h_helper n)^.elt_of
 
   private lemma foo (a : ℕ) : h a < h (succ a) ∧ (fst ∘ f) (h a) ≤ (fst ∘ f) (h (succ a)) := 
-  have ¬ terminal wqo.le (fst ∘ f) (h a), from and.right (has_property (h_helper a)),
+  have ¬ terminal wqo.le (fst ∘ f) (h a), from (has_property $ h_helper a)^.right,
   have ∃ n, (h a) < n ∧ ((fst ∘ f) (h a)) ≤ ((fst ∘ f) n), from prop_of_non_terminal this,
-  some_spec this
+  show _, from some_spec this
 
   theorem property_of_h {i j : ℕ} : i < j → (fst ∘ f) (h i) ≤ (fst ∘ f) (h j) :=
-  nat.induction_on j
-  (assume H, absurd H dec_trivial)
+  nat.rec_on j (assume H, absurd H dec_trivial)
   (take a, assume IH, assume lt,
   have H1 : (fst ∘ f) (h a) ≤ (fst ∘ f) (h (succ a)), from (foo a)^.right,
   have disj : i < a ∨ i = a, from lt_or_eq_of_lt_succ lt,
-  or.elim disj
-  (λ Hl, wqo.trans (IH Hl) H1)
-  (λ Hr, by simp [Hr, H1])) 
+  or.elim disj (λ Hl, wqo.trans (IH Hl) H1) (λ Hr, by simp [Hr, H1])) 
 
   theorem increasing_h {i j : ℕ} : i < j → h i < h j :=
-  nat.induction_on j
+  nat.rec_on j
   (assume H, absurd H dec_trivial)
   (take a, assume IH, assume lt,
-  have H1 : (h a) < h (succ a), from and.left (foo a),
+  have H1 : (h a) < h (succ a), from (foo a)^.left,
   have disj : i < a ∨ i = a, from lt_or_eq_of_lt_succ lt,
-  or.elim disj
-  (λ Hl, lt.trans (IH Hl) H1)
-  (λ Hr, by simp [Hr, H1]))
+  or.elim disj (λ Hl, lt.trans (IH Hl) H1) (λ Hr, by simp [Hr, H1]))
 
   theorem good_f : is_good f (o_for_pairs wqo.le wqo.le) :=
   have ∃ i j : ℕ, i < j ∧ (snd ∘ f ∘ h) i ≤ (snd ∘ f ∘ h) j, from wqo.is_good (snd ∘ f ∘ h),
   let ⟨i,j,H⟩ := this in
-  have (fst ∘ f) (h i) ≤ (fst ∘ f) (h j), from property_of_h (and.left H),
-  have Hr : (fst ∘ f) (h i) ≤ (fst ∘ f) (h j) ∧ (snd ∘ f) (h i) ≤ (snd ∘ f) (h j), from and.intro this (and.right H),
-  have h i < h j, from increasing_h (and.left H),
-  have h i < h j ∧ (fst ∘ f) (h i) ≤ (fst ∘ f) (h j) ∧ (snd ∘ f) (h i) ≤ (snd ∘ f) (h j), from and.intro this Hr,
-  ⟨(h i), (h j), this⟩
+  have (fst ∘ f) (h i) ≤ (fst ∘ f) (h j), from property_of_h H^.left,
+  have Hr : (fst ∘ f) (h i) ≤ (fst ∘ f) (h j) ∧ (snd ∘ f) (h i) ≤ (snd ∘ f) (h j), from and.intro this H^.right,
+  have h i < h j, from increasing_h H^.left,
+  ⟨(h i), (h j), ⟨this,Hr⟩⟩
 
   end
   
-theorem good_pairs : ∀ f : ℕ → A × B, is_good f (o_for_pairs wqo.le wqo.le) := good_f
+theorem good_pairs (f : ℕ → A × B) : is_good f (o_for_pairs wqo.le wqo.le) := good_f f
 
 end
 
 instance wqo_prod  {A B : Type} [HA : wqo A] [HB : wqo B] : wqo (A × B) :=
 let op : A × B → A × B → Prop := o_for_pairs wqo.le wqo.le in
 have refl : ∀ p : A × B, op p p, by intro; apply and.intro;repeat {apply wqo.refl},
-have trans : ∀ a b c, op a b → op b c → op a c,
-  begin intros x y z h1 h2, apply and.intro,
-  apply wqo.trans  (and.left h1) (and.left h2),
-  apply wqo.trans (and.right h1) (and.right h2),
-  end,
-wqo.mk op refl trans good_pairs
+have trans : ∀ a b c, op a b → op b c → op a c, from λ a b c h1 h2, 
+  ⟨wqo.trans h1^.left h2^.left, wqo.trans h1^.right h2^.right⟩,
+show _, from wqo.mk op refl trans good_pairs
 
 end kruskal
